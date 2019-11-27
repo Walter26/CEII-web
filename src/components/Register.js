3d6c3bde8c7cb9103783e4aa8ecea3bb1d099f06
MiguelPortillo18 <00043818@uca.edu.sv>
2019-11-26 19:04:14 -0600

    Add: Register media query

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,36 +7,39 @@ class Register extends React.Component{
             <div className="container register">
                 <div className="box reg">
                     <div className="pair">
-                        <label htmlFor="email">Correo electrónico</label>
+                        <label htmlFor="email">Correo electrónico:</label>
                         <input type="text" name="email" id="email"/>
                     </div>
                     <div className="pair">
-                        <label htmlFor="usrname">Nombre de usuario</label>
+                        <label htmlFor="usrname">Nombre de usuario:</label>
                         <input type="text" name="usrname" id="usrname"/>
                     </div>
                     <div className="pair">
-                        <label htmlFor="fullname">Nombre Completo</label>
+                        <label htmlFor="fullname">Nombre Completo:</label>
                         <input type="text" name="fullname" id="fullname"/>
                     </div>
                     <div className="pair">
-                        <label htmlFor="phone">Teléfono</label>
+                        <label htmlFor="phone">Teléfono:</label>
                         <input type="text" name="phone" id="phone"/>
                     </div>
                     <div className="pair">
-                        <label htmlFor="gender">Género</label>
+                        <label htmlFor="gender">Género:</label>
                         <select name="gender" id="gender">
                             <option value="F">Femenino</option>
                             <option value="M">Másculino</option>
                         </select>
                     </div>
                     <div className="pair">
-                        <label htmlFor="password">Contraseña</label>
-                        <input type="password" name="password" id="password"/>
+                        <label htmlFor="password">Contraseña:</label>
+                        <input type="password" name="password" id="password"/> 
                         <p>Debe contener una mayúscula y un número, no puede exceder 15 caracteres</p>
                     </div>
                     <div className="pair">
-                        <label htmlFor="cPassword">Confirmar contraseña</label>
+                        <label htmlFor="cPassword">Confirmar contraseña:</label>
                         <input type="password" name="cPassword" id="cPassword"/>
+                    </div>
+                    <div className="pair">
+                        <button type="submit" name="submit" id="submit">Registrarse</button>
                     </div>
                 </div>
             </div>
